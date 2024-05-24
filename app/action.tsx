965421d6bdaebc86bbc61b813e84122cc38363ea
admin lion <admin@admin.com>
2024-05-24 15:38:44 +0900

    nomad-test : zod 사용

--- app/action.tsx
+++ app/action.tsx
@@ -1,17 +1,30 @@
 "use server";
 
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z
+    .string()
+    .email()
+    .regex(/@zod\.com$/, "이메일은 @zod.com으로 끝나야 합니다."),
+
+  name: z.string().min(5, "Name은 5글자 이상이어야 합니다."),
+  password: z
+    .string()
+    .min(10, "비번은 10글자 이상이어야 합니다.")
+    .regex(/\d/, "비번에는 적어도 하나의 숫자가 포함되어야 합니다."),
+});
 export const handleForm = async (prevState: any, formData: FormData) => {
-  await new Promise((resolve) => {
-    setTimeout(resolve, 2000);
-  });
-  const password = formData.get("password");
-  if (password !== "12345") {
+  const data = {
+    email: formData.get("email"),
+    name: formData.get("name"),
+    password: formData.get("password"),
+  };
+
+  const result = formSchema.safeParse(data);
+  if (!result.success) {
     return {
-      errors: ["Wrong password"],
-    };
-  } else {
-    return {
-      success: true,
+      errors: result.error.flatten().fieldErrors,
     };
   }
 };
--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,16 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <form action={action} className="flex flex-col gap-4">
-          <Input type="email" placeholder="Enter your email" name="email" />
-          <Input type="text" placeholder="Enter your name" name="name" />
-          <Input type="password" placeholder="Enter your password" name="password" errors={state?.errors ?? []} />
+          <Input type="email" placeholder="Enter your email" name="email" errors={state?.errors.email ?? []} />
+          <Input type="text" placeholder="Enter your name" name="name" errors={state?.errors.name ?? []} />
+          <Input
+            type="password"
+            placeholder="Enter your password"
+            name="password"
+            errors={state?.errors.password ?? []}
+          />
           <Button text="Login" />
         </form>
-        {state?.success ? <div className="bg-green-500 rounded-lg p-2 mt-2 font-blod text-black">Success</div> : null}
       </div>
     </main>
   );
